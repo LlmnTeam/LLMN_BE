281604b7a9ce6f4f4598d9c46accd36ca49ca94e
hoyaii <131665728+hoyaii@users.noreply.github.com>
2024-08-17 15:03:54 +0900

    feat: UserService 클래스를 생성하고 login() 메서드 구현

---
+++ src/main/java/com/example/llmn/service/UserService.java
@@ -0,0 +1,57 @@
+package com.example.llmn.service;
+
+import com.example.llmn.core.errors.CustomException;
+import com.example.llmn.core.errors.ExceptionCode;
+import com.example.llmn.core.security.JWTProvider;
+import com.example.llmn.domain.User;
+import com.example.llmn.repository.UserRepository;
+import jakarta.mail.MessagingException;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.*;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class UserService {
+
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final RedisService redisService;
+
+    @Transactional
+    public Map<String, String> login(UserRequest.LoginDTO requestDTO, HttpServletRequest request) throws MessagingException {
+        User user = userRepository.findByEmail(requestDTO.email()).orElseThrow(
+                () -> new CustomException(ExceptionCode.USER_ACCOUNT_WRONG)
+        );
+
+        // 비밀번호가 일치하지 않음
+        if(!passwordEncoder.matches(requestDTO.password(), user.getPassword())){
+            throw new CustomException(ExceptionCode.USER_ACCOUNT_WRONG);
+        }
+
+        return createToken(user);
+    }
+
+    private Map<String, String> createToken(User user){
+        String accessToken = JWTProvider.createAccessToken(user);
+        String refreshToken = JWTProvider.createRefreshToken(user);
+
+        // Access Token 갱신
+        redisService.storeValue("accessToken", String.valueOf(user.getId()), accessToken, JWTProvider.ACCESS_EXP_MILLI);
+
+        // Refresh Token 갱신
+        redisService.storeValue("refreshToken", String.valueOf(user.getId()), refreshToken, JWTProvider.REFRESH_EXP_MILLI);
+
+        // Map으로 토큰들을 담아 반환
+        Map<String, String> tokens = new HashMap<>();
+        tokens.put("accessToken", accessToken);
+        tokens.put("refreshToken", refreshToken);
+
+        return tokens;
+    }
+}
