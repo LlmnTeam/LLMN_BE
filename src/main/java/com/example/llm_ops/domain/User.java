fa5799441e92d3f006e72a70a4552d5a043b284c
hoyaii <131665728+hoyaii@users.noreply.github.com>
2024-08-17 14:27:34 +0900

    feat: User 엔티티 클래스 구현

---
+++ src/main/java/com/example/llm_ops/domain/User.java
@@ -0,0 +1,40 @@
+package com.example.llm_ops.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user_tb")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class User extends TimeStamp {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String nickName;
+
+    @Column
+    private String email;
+
+    @Column
+    private String password;
+
+
+    @Builder
+    public User(Long id, String nickName, String email, String password) {
+        this.id = id;
+        this.nickName = nickName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public void updatePassword (String password) {
+        this.password  = password;
+    }
+}
