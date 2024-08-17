53612bd559b12b0c4740f3db28d8fcc78a15a9bb
hoyaii <131665728+hoyaii@users.noreply.github.com>
2024-08-17 14:56:26 +0900

    feat: CookieUtils 클래스 구현

--- src/main/java/com/example/llmn/core/security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/llmn/core/security/JwtAuthenticationFilter.java
@@ -4,6 +4,7 @@ import com.auth0.jwt.exceptions.JWTDecodeException;
 import com.auth0.jwt.exceptions.SignatureVerificationException;
 import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import com.example.llmn.core.utils.CookieUtils;
 import com.example.llmn.domain.User;
 import com.example.llmn.domain.UserRole;
 import com.example.llmn.service.RedisService;
