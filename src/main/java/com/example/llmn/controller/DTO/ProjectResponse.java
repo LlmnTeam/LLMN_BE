9087e4b609ea796216f86629de76fa6ab5025576
hoyaii <131665728+hoyaii@users.noreply.github.com>
2024-09-10 15:51:32 +0900

    feat: ProjectService에 findProjectList() 메서드 구현

---
+++ src/main/java/com/example/llmn/controller/DTO/ProjectResponse.java
@@ -0,0 +1,17 @@
+package com.example.llmn.controller.DTO;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class ProjectResponse {
+
+    public record FindProjectListDTO(List<ProjectDTO> projects){}
+
+    public record ProjectDTO(
+            String name,
+            String description,
+            LocalDateTime updateTime) {}
+}
--- src/main/java/com/example/llmn/service/ProjectService.java
+++ src/main/java/com/example/llmn/service/ProjectService.java
@@ -1,6 +1,7 @@
 package com.example.llmn.service;
 
 import com.example.llmn.controller.DTO.ProjectRequest;
+import com.example.llmn.controller.DTO.ProjectResponse;
 import com.example.llmn.domain.Project;
 import com.example.llmn.domain.User;
 import com.example.llmn.repository.ProjectRepository;
@@ -9,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -29,5 +32,16 @@ public class ProjectService {
                 .build();
 
         projectRepository.save(project);
+    }
+
+    @Transactional
+    public ProjectResponse.FindProjectListDTO findProjectList(Long userId){
+        List<Project> projects = projectRepository.findByUserId(userId);
+
+        List<ProjectResponse.ProjectDTO> projectDTOS = projects.stream()
+                .map(project -> new ProjectResponse.ProjectDTO(project.getName(), project.getDescription(), project.getUpdatedDate()))
+                .toList();
+
+        return new ProjectResponse.FindProjectListDTO(projectDTOS);
     }
 }
