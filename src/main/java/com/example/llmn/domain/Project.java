2e9f37304b5b6a8b853735e321f1ff3ee2c30352
hoyaii <131665728+hoyaii@users.noreply.github.com>
2024-09-10 14:22:03 +0900

    fix: Project 엔티티 필드에 user 추가

--- src/main/java/com/example/llmn/domain/Project.java
+++ src/main/java/com/example/llmn/domain/Project.java
@@ -2,6 +2,7 @@ package com.example.llmn.domain;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,12 +10,26 @@ import lombok.NoArgsConstructor;
 @Table(name = "project_tb")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class Project {
+public class Project extends TimeStamp{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
     @Column
     private String name;
+
+    @Column
+    private String description;
+
+    @Builder
+    public Project(User user, String name, String description) {
+        this.user = user;
+        this.name = name;
+        this.description = description;
+    }
 }
