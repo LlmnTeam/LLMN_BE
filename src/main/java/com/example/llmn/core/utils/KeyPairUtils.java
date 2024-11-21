71ee620a3f4728f27f0b0995616f603b112d8586
hoyaii <131665728+hoyaii@users.noreply.github.com>
2024-11-21 19:57:47 +0900

    refactor: KeyPairUtils 클래스의 loadKeyPair() 메서드에서 키 파일을 검증하는 로직과 키 페어를 파싱하는 로직을 메서드로 추출

--- src/main/java/com/example/llmn/core/utils/KeyPairUtils.java
+++ src/main/java/com/example/llmn/core/utils/KeyPairUtils.java
@@ -3,7 +3,6 @@ package com.example.llmn.core.utils;
 import org.apache.sshd.common.NamedResource;
 import org.apache.sshd.common.config.keys.FilePasswordProvider;
 import org.apache.sshd.common.config.keys.loader.KeyPairResourceParser;
-import org.apache.sshd.common.session.SessionContext;
 import org.apache.sshd.common.util.GenericUtils;
 import org.apache.sshd.common.util.security.SecurityUtils;
 
@@ -14,39 +13,46 @@ import java.util.Collection;
 
 public class KeyPairUtils {
 
-    public static KeyPair loadKeyPair(String privateKeyPath) throws IOException, GeneralSecurityException {
-        File privateKeyFile = new File(privateKeyPath);
+    private KeyPairUtils() {}
 
-        // 해당 경로에 파일이 존재하지 않음
-        if (!privateKeyFile.exists()) {
-            throw new IOException("Pem 키 파일이 존재하지 않음: " + privateKeyPath);
+    private static final FilePasswordProvider DEFAULT_PASSWORD_PROVIDER = FilePasswordProvider.EMPTY;
+
+    public static KeyPair loadKeyPair(String pemKeyPath) throws IOException, GeneralSecurityException {
+        File pemKey = validatePemKey(pemKeyPath);
+
+        try (InputStream inputStream = new FileInputStream(pemKey)) {
+            return parseKeyPair(pemKey, inputStream);
+        }
+    }
+
+    private static File validatePemKey(String pemKeyPath) throws IOException {
+        File pemKey = new File(pemKeyPath);
+
+        if (!pemKey.exists()) {
+            throw new IOException("Pem 키 파일이 존재하지 않음: " + pemKeyPath);
         }
 
-        // 파일 입력 스트림을 열어 키 파일을 읽어들임
-        try (InputStream inputStream = new FileInputStream(privateKeyFile)) {
-            NamedResource resourceKey = new NamedResource() { // NamedResource는 SSHD 라이브러리에서 파일의 이름을 나타내는 인터페이스
-                @Override
-                public String getName() {
-                    return privateKeyFile.getName();
-                }
-            };
+        return pemKey;
+    }
 
-            FilePasswordProvider provider = FilePasswordProvider.EMPTY; // 패스프레이즈 없어서 EMPTY로
-            SessionContext session = null; // 특정 세션이 필요하지 않아서 null로 설정
+    private static KeyPair parseKeyPair(File pemKey, InputStream inputStream) throws GeneralSecurityException, IOException {
+        NamedResource resourceKey = pemKey::getName;
 
-            // SecurityUtils에서 KeyPairResourceParser를 가져옴
-            KeyPairResourceParser parser = SecurityUtils.getKeyPairResourceParser();
-            if (parser == null) {
-                throw new GeneralSecurityException("등록된 key-pair 파서가 존재하지 않음.");
-            }
+        KeyPairResourceParser parser = SecurityUtils.getKeyPairResourceParser();
+        Collection<KeyPair> keyPairs = parser.loadKeyPairs(null, resourceKey, DEFAULT_PASSWORD_PROVIDER, inputStream);
 
-            // 파서를 사용해서, 키 파일을 읽어 KeyPair 객체들을 로드
-            Collection<KeyPair> keyPairs = parser.loadKeyPairs(session, resourceKey, provider, inputStream);
-            if (GenericUtils.isEmpty(keyPairs)) {
-                throw new IOException("keyPair 로드 실패: " + privateKeyPath);
-            }
+        validateKeyPairs(keyPairs);
 
-            return keyPairs.iterator().next(); // 첫 번째 KeyPair 반환
+        return getFirstKeyPair(keyPairs);
+    }
+
+    private static void validateKeyPairs(Collection<KeyPair> keyPairs) throws IOException {
+        if (GenericUtils.isEmpty(keyPairs)) {
+            throw new IOException("SSH 접속 시 필요한 KeyPair 로드 실패");
         }
+    }
+
+    private static KeyPair getFirstKeyPair(Collection<KeyPair> keyPairs) {
+        return keyPairs.iterator().next();
     }
 }
