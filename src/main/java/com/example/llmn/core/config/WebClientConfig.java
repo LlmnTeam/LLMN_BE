a2e2a220803db3c8c1688f6eb3e90ea6ff01c193
hoyaii <131665728+hoyaii@users.noreply.github.com>
2024-11-28 09:00:42 +0900

    refactor: WebClientConfig의 webClient() 메서드의 로직을 쪼개서 메서드로 추출

--- src/main/java/com/example/llmn/core/config/WebClientConfig.java
+++ src/main/java/com/example/llmn/core/config/WebClientConfig.java
@@ -8,45 +8,61 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ClientHttpConnector;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
-import org.springframework.http.client.reactive.ReactorResourceFactory;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
+import reactor.netty.resources.ConnectionProvider;
+import reactor.netty.resources.LoopResources;
 
 import java.time.Duration;
-import java.util.function.Function;
 
 @Configuration
 public class WebClientConfig {
 
-    @Bean
-    public ReactorResourceFactory resourceFactory() {
-        ReactorResourceFactory factory = new ReactorResourceFactory();
-        factory.setUseGlobalResources(false);
-        return factory;
-    }
+    private static final int CONNECT_TIMEOUT = 100_000; // 100초
+    private static final int READ_WRITE_TIMEOUT = 60; // 60초
+    private static final int BUFFER_SIZE_MB = 1024 * 1024; // 1MB
 
     @Bean
-    public WebClient webClient() {
-        // 1MB
-        int bufferSize = 1024 * 1024;
-
-        ExchangeStrategies.Builder exchangeStrategiesBuilder = ExchangeStrategies.builder();
-        exchangeStrategiesBuilder.codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(bufferSize));
-        ExchangeStrategies exchangeStrategies = exchangeStrategiesBuilder.build();
-
-        Function<HttpClient, HttpClient> mapper = client -> HttpClient.create()
-                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 100000)
-                .doOnConnected(connection ->
-                        connection.addHandlerLast(new ReadTimeoutHandler(60))
-                                .addHandlerLast(new WriteTimeoutHandler(60)))
-                .responseTimeout(Duration.ofSeconds(60));
-
-        ClientHttpConnector connector = new ReactorClientHttpConnector(resourceFactory(), mapper);
-
+    public WebClient webClient(ClientHttpConnector connector, ExchangeStrategies exchangeStrategies) {
         return WebClient.builder()
                 .exchangeStrategies(exchangeStrategies)
                 .clientConnector(connector)
+                .build();
+    }
+
+    @Bean
+    public ClientHttpConnector clientHttpConnector(HttpClient httpClient) {
+        return new ReactorClientHttpConnector(httpClient);
+    }
+
+    @Bean
+    public HttpClient httpClient(ConnectionProvider connectionProvider, LoopResources loopResources) {
+        return HttpClient.create(connectionProvider)
+                .runOn(loopResources)
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, CONNECT_TIMEOUT)
+                .doOnConnected(connection ->
+                        connection.addHandlerLast(new ReadTimeoutHandler(READ_WRITE_TIMEOUT))
+                                .addHandlerLast(new WriteTimeoutHandler(READ_WRITE_TIMEOUT)))
+                .responseTimeout(Duration.ofSeconds(60));
+    }
+
+    @Bean(destroyMethod = "dispose")
+    public ConnectionProvider connectionProvider() {
+        return ConnectionProvider.create("customConnectionProvider");
+    }
+
+    @Bean(destroyMethod = "dispose")
+    public LoopResources loopResources() {
+        return LoopResources.create("cuomLoopResources");
+    }
+
+    @Bean
+    public ExchangeStrategies exchangeStrategies() {
+        return ExchangeStrategies.builder()
+                .codecs(configurer -> configurer
+                        .defaultCodecs()
+                        .maxInMemorySize(BUFFER_SIZE_MB))
                 .build();
     }
 }
